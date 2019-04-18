7881b808345926798dbe74d8b2d14f6d24373d4a
Christopher Smith <csmith4work@gmail.com>
2019-04-17 23:00:56 -0500

    Initializing compoents and preparing for EF.

---
+++ BlackPositivity.Services/Models/BlackPositivtyQuote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackPositivity.Services.Models
+{
+    public class BlackPositivtyQuote
+    {
+        public Guid ID { get; set; }
+        public string Contributor { get; set; }
+        public string Quote { get; set; }
+        public bool hasBeenUsed { get; set; }
+    }
+}
