b76643c5e11322cfcded9f382de4d70b713b063b
Christopher Smith <csmith4work@gmail.com>
2019-04-24 19:06:58 -0500

    Disregard.

---
+++ BlackPositivity.Services/Dtos/QuoteSubmissionDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackPositivity.Services.Dtos
+{
+    public class QuoteSubmissionDto
+    {
+        public string Contributor { get; set; }
+        public string Quote { get; set; }
+    }
+}
--- BlackPositivitySubmissionSite/wwwroot/js/site.js
+++ BlackPositivitySubmissionSite/wwwroot/js/site.js
@@ -1,1 +1,17 @@
-﻿
+﻿$(document).ready(function () {
+    $("#submission").submit(function (event) {
+        event.preventDefault();
+        let submission = $(this).serializeArray();
+        console.log(submission);
+        $.ajax({
+            url: "api/BlackPositivityQuote/newQuote",
+            type: "post",
+            data: JSON.stringify(submission),
+            contentType: 'application/json; charset=utf-8',
+            dataType: "json",
+            success: function (response) {
+                console.log(response.data);
+            }
+        })
+    });
+});
